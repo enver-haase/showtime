2e04ed468e997eac14ecc7ab3956857a6a94c677
Enver Haase <enver@vaadin.com>
2017-03-23 19:59:02 +0100

    remove the Windows workaround for the broken Jetty plugin.

--- windows-workaround.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Some operating systems are more equal than others.
-
-start call windows-start-tests.bat
-
-SET MAVEN_OPTS=-Ddatabase.backend=com.infraleap.demo.tests.database.TestDB
-mvn -Pci-tests jetty:run
